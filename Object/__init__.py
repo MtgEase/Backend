26affbc7c1776c55ef754983603da05b080c6148
zhangxinhui02 <zhangxinhui02@qq.com>
2024-03-17 00:49:35 +0800

    feat: 新增验证给定的UUID是否已存在功能

--- Object/__init__.py
+++ Object/__init__.py
@@ -4,6 +4,7 @@
 为了保证同步，数据全部实时存储在数据库中，实例属性均为动态加载，而对上层屏蔽这一过程。
 """
 from Database import Database
+from uuid import UUID
 
 
 class Object:
@@ -15,3 +16,21 @@ class Object:
     rooms = {}
     meetings = {}
     determines = {}
+
+    @staticmethod
+    def is_uuid_exist(uuid: str | UUID) -> bool:
+        """验证给定的UUID是否已存在"""
+        uuid = str(uuid)
+        if uuid in Object.groups:
+            return True
+        if uuid in Object.tags:
+            return True
+        if uuid in Object.users:
+            return True
+        if uuid in Object.rooms:
+            return True
+        if uuid in Object.meetings:
+            return True
+        if uuid in Object.determines:
+            return True
+        return False
