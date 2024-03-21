33b1d516a1de3b140d65f9279caef0068246338f
zhangxinhui02 <zhangxinhui02@qq.com>
2024-03-21 19:27:18 +0800

    change: 增加类型提示

--- Object/__init__.py
+++ Object/__init__.py
@@ -3,34 +3,42 @@
 
 为了保证同步，数据全部实时存储在数据库中，实例属性均为动态加载，而对上层屏蔽这一过程。
 """
+from typing import Dict
 from Database import Database
-from uuid import UUID
+import uuid as uuid_pkg
+from Model import uuid
+from .determine import Determine
+from .group import Group
+from .meeting import Meeting
+from .room import Room
+from .tag import Tag
+from .user import User
 
 
 class Manager:
     """管理并存储实例的静态类"""
     db = Database()
-    groups = {}
-    tags = {}
-    users = {}
-    rooms = {}
-    meetings = {}
-    determines = {}
+    groups: Dict[uuid, Group] = {}
+    tags: Dict[uuid, Tag] = {}
+    users: Dict[uuid, User] = {}
+    rooms: Dict[uuid, Room] = {}
+    meetings: Dict[uuid, Meeting] = {}
+    determines: Dict[uuid, Determine] = {}
 
     @staticmethod
-    def is_uuid_exist(uuid: str | UUID) -> bool:
+    def is_uuid_exist(item_id: str | uuid_pkg.UUID) -> bool:
         """验证给定的UUID是否已存在"""
-        uuid = str(uuid)
-        if uuid in Manager.groups:
+        item_id = str(item_id)
+        if item_id in Manager.groups:
             return True
-        if uuid in Manager.tags:
+        if item_id in Manager.tags:
             return True
-        if uuid in Manager.users:
+        if item_id in Manager.users:
             return True
-        if uuid in Manager.rooms:
+        if item_id in Manager.rooms:
             return True
-        if uuid in Manager.meetings:
+        if item_id in Manager.meetings:
             return True
-        if uuid in Manager.determines:
+        if item_id in Manager.determines:
             return True
         return False
